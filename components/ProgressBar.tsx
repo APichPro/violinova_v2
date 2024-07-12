fe2bfc666ff95b5afb922d2504affa8d6ec1d424
Aristide <apichereau.pro@gmail.com>
2024-07-12 21:01:57 +0200

    Update

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,14 +1,23 @@
-import { SynthProvider } from "@/providers/SynthProvider";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useSynth } from "@/providers/SynthProvider";
 
-const ProgressBar = () => {
-  const [first, setfirst] = useState(SynthProvider.getProgress());
-  console.log(first);
-  return (
-    <div className="bg-red-500 z-10 w-full h-24">
-      <div className={`bg-green-500 h-24 w-[${first}]`}></div>
-    </div>
-  );
+const ProgressComponent = () => {
+  const { subscribeToProgress } = useSynth();
+  const [progress, setProgress] = useState<number>(0);
+
+  useEffect(() => {
+    const handleProgressUpdate = (newProgress: number) => {
+      setProgress(newProgress);
+    };
+
+    subscribeToProgress(handleProgressUpdate);
+
+    return () => {
+      // Clean up if necessary (unsubscribe logic)
+    };
+  }, [subscribeToProgress]);
+
+  return <div>Progress: {Math.round(progress * 100)}%</div>;
 };
 
-export default ProgressBar;
+export default ProgressComponent;
